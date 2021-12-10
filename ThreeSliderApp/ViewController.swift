35106fe2666518452beb29375b3cea82fa8cc2c3
vic8tor <timongrinni@gmail.com>
2021-12-11 00:43:02 +0300

    Fix  rgb action

--- ThreeSliderApp/ViewController.swift
+++ ThreeSliderApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var topView: UIView!
-    @IBOutlet weak var redSliderLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
     @IBOutlet weak var greenValueLabel: UILabel!
     @IBOutlet weak var blueValueLabel: UILabel!
     @IBOutlet weak var redSlider: UISlider!
@@ -19,12 +19,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redValueLabel.text = String(redSlider.value)
+        greenValueLabel.text = String(greenSlider.value)
+        blueValueLabel.text = String(blueSlider.value)
+
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
     }
-    
     override func viewWillLayoutSubviews() {
         topView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func moveSlider() {
+        redValueLabel.text = String(redSlider.value.rounded())
+        greenValueLabel.text = String(greenSlider.value.rounded())
+        blueValueLabel.text = String(blueSlider.value.rounded())
+        topView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    
+    }
 }
 
